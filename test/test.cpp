2ac8bf8bff3f577030281a317d10c457e71a161d
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2016-12-04 18:47:27 +0100

    Test loadLibrary

--- test/test.cpp
+++ test/test.cpp
@@ -2,17 +2,34 @@
 #include "catch.hpp"
 #include "LibManager.hpp"
 
-TEST_CASE("findLibrary", "[libmanager]")
+
+TEST_CASE("existing library", "[loadLibrary]")
 {
-	lib_manager::LibManager libman;
-	std::string libName = libman.findLibrary("abcd");
-	REQUIRE(std::string("lib").compare(0, 3, libName));
-	REQUIRE(std::string("abcd").compare(3, 7, libName));
+    lib_manager::LibManager libman;
+    lib_manager::LibManager::ErrorNumber error = libman.loadLibrary("abcd");
+    REQUIRE(error == lib_manager::LibManager::LIBMGR_NO_ERROR);
 }
 
-TEST_CASE("loadLibrary", "[libmanager]")
+TEST_CASE("unknown library", "[loadLibrary]")
 {
-	lib_manager::LibManager libman;
-	lib_manager::LibManager::ErrorNumber error = libman.loadLibrary("abcd");
-	REQUIRE(error == lib_manager::LibManager::LIBMGR_NO_ERROR);
+    lib_manager::LibManager libman;
+    lib_manager::LibManager::ErrorNumber error = libman.loadLibrary("unknown");
+    REQUIRE(error == lib_manager::LibManager::LIBMGR_ERR_NOT_ABLE_TO_LOAD);
+}
+
+TEST_CASE("twice", "[loadLibrary]")
+{
+    lib_manager::LibManager libman;
+    lib_manager::LibManager::ErrorNumber error = libman.loadLibrary("abcd");
+    REQUIRE(error == lib_manager::LibManager::LIBMGR_NO_ERROR);
+    error = libman.loadLibrary("abcd");
+    REQUIRE(error == lib_manager::LibManager::LIBMGR_ERR_LIBNAME_EXISTS);
+}
+
+TEST_CASE("check library name", "[findLibrary]")
+{
+    lib_manager::LibManager libman;
+    std::string libName = libman.findLibrary("abcd");
+    REQUIRE(std::string("lib").compare(0, 3, libName));
+    REQUIRE(std::string("abcd").compare(3, 7, libName));
 }

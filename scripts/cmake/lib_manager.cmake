f27fec2232f927a89f9ba68c654afdf29eedc83c
Malte Langosz <malte.langosz@dfki.de>
2017-08-03 19:23:12 +0200

    Set c++11 only for cpp compiler

--- scripts/cmake/lib_manager.cmake
+++ scripts/cmake/lib_manager.cmake
@@ -52,7 +52,7 @@ macro(lib_defaults)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
   endif()
 
-  add_definitions(-std=c++11)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 endmacro(lib_defaults)
 
 

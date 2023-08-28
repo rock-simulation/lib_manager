8a7405a6d06b9a00b10e7a52a853f6b0c5b6dc33
Anna Born <anna.born@dfki.de>
2023-08-29 00:24:40 +0200

    install lib in its own folder and remove deprecated headers

--- src/LibInterface.h
+++
@@ -1,2 +0,0 @@
-#warning "The include <mars/lib_manager/LibInterface.h> is deprecated! User <lib_manager/LibInterface.hpp> instead."
-#include <lib_manager/LibInterface.hpp>
--- src/LibManager.h
+++
@@ -1,2 +0,0 @@
-#warning "The include <mars/lib_manager/LibManager.h> is deprecated! User <lib_manager/LibManager.hpp> instead."
-#include <lib_manager/LibManager.hpp>
